c5ae6d549025687668aebe8287a186e925d56402
skenoy <skenoy@gmail.com>
2025-10-31 15:56:35 +0800

    postgres 初始化方法

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from tortoise import Tortoise
 
 async def init_db():
-    await Tortoise.init(db_url='sqlite:////home/suny/agent/fapi/db.sqlite3',modules={'models': ['models']}, timezone='Asia/Shanghai')
+    await Tortoise.init(db_url='postgres://ai:ai@localhost:5432/users',modules={'models': ['models']}, timezone='Asia/Shanghai')
     await Tortoise.generate_schemas()
 
 async def close_db():
