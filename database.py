6484b4255904df00546a8bbabf93586a15ff74ac
skenoy <skenoy@gmail.com>
2025-12-11 14:55:02 +0800

    更新数据库初始化方法，修正数据库连接字符串并启用安全模式生成模式

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from tortoise import Tortoise
 
 async def init_db():
-    await Tortoise.init(db_url='postgres://ai:ai@localhost:5432/users',modules={'models': ['models']}, timezone='Asia/Shanghai')
-    await Tortoise.generate_schemas()
+    await Tortoise.init(db_url='postgres://ai:ai@localhost:5432/xxx',modules={'models': ['models']}, timezone='Asia/Shanghai')
+    await Tortoise.generate_schemas(safe=True)
 
 async def close_db():
     await Tortoise.close_connections()
