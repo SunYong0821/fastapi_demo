59e48f4af876355419a89e665333651eaee4cf04
skenoy <skenoy@gmail.com>
2026-05-11 17:14:50 +0800

    优化数据库模型的函数

--- models.py
+++ models.py
@@ -2,6 +2,22 @@ from tortoise.models import Model
 from tortoise import fields
 from datetime import datetime
 
+def model_to_dict(s, exclude=None):
+    res = {}
+    for i in s._meta.fields_map.keys():
+        if exclude and i in exclude:
+            continue
+        val = getattr(s, i)
+        if isinstance(val, datetime):
+            val = val.strftime('%Y-%m-%d %H:%M:%S')
+        res[i] = val
+    return res
+
+def model_update(model, **kwargs):
+    for k, v in kwargs.items():
+        setattr(model, k, v)
+    return model
+
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=30, unique=True, index=True)
@@ -13,14 +29,7 @@ class User(Model):
         table = "users"
 
     def to_dict(self):
-        json = {}
-        for i in self._meta.fields_map.keys():
-            val = getattr(self, i)
-            if isinstance(val, datetime):
-                val = val.strftime('%Y-%m-%d %H:%M:%S')
-            json[i] = val
-        return json
+        return model_to_dict(self, exclude=['password'])
     
     def update(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+        return model_update(self, **kwargs)
